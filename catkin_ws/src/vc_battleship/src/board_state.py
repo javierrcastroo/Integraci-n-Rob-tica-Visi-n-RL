80ca75e04f38e9318ff90a91d966289ff9048c09
Javier Castro Caballero <javiercastro010601@gmail.com>
2025-11-12 19:36:48 +0100

    Refinar barcos y confirmación de gestos

--- catkin_ws/src/vc_battleship/src/board_state.py
+++ catkin_ws/src/vc_battleship/src/board_state.py
@@ -16,7 +16,7 @@ def init_board_state(name):
     }
 
 
-# origen global = el cubo verde que el usuario calibra con 'r'
+# origen global = centro del marcador ArUco detectado en la escena
 # lo vamos actualizando cada frame
 GLOBAL_ORIGIN = None       # (x, y) en píxeles
 GLOBAL_ORIGIN_MISS = 0     # para aguantar unos frames si desaparece
