45a0b12c8166e9c51684d535027fd7fb7ac74956
Juan <145612486+JuanArizaCoding@users.noreply.github.com>
2025-11-27 20:12:34 +0100

    Update env_register.py
    kdjfasdfdkl

--- RL/src/RL/env_register.py
+++ RL/src/RL/env_register.py
@@ -1,10 +1,20 @@
-from gym.envs.registration import register
+from gymnasium.envs.registration import register
+import rospkg
+import sys
+import os
 
 def register_env():
+    rospack = rospkg.RosPack()
+    RL_PATH = rospack.get_path("RL") + "src/RL"
+    
+    if RL_PATH not in sys.path:
+        sys.path.append(RL_PATH)
+    
     try:
         register(
             id="BattleshipROS-v0",
-            entry_point="RL.battleship_mock__env:BattleshipMockROSEnv",
+            entry_point="battleship_mock_env:BattleshipMockROSEnv",
+
             max_episode_steps=25,  # 5x5 → 25 disparos como máximo
         )
     except Exception:
